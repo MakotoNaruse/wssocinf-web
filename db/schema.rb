5c656bf0816aba0fe736d14446b78d3200bf2862
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-07-16 17:37:58 +0900

    最後のchange situationからのtimeをuser idenfityに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_070405) do
+ActiveRecord::Schema.define(version: 2020_07_16_080932) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_07_09_070405) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "situation", default: 0
+    t.datetime "last_recieved_time", null: false
   end
 
   add_foreign_key "comments", "articles"
