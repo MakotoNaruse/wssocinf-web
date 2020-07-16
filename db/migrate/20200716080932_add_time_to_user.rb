7f49f4263f2fbfdc1b5280a44f0b9272e3262fc0
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-07-16 17:45:48 +0900

    fix

--- db/migrate/20200716080932_add_time_to_user.rb
+++ db/migrate/20200716080932_add_time_to_user.rb
@@ -2,7 +2,7 @@ class AddTimeToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :last_recieved_time, :datetime, null: true
 
-    User.update_all(created_at: Time.current)
+    User.update_all(last_recieved_time: Time.current)
 
     change_column :users, :last_recieved_time, :datetime, null: false
   end
